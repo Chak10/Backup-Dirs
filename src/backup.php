c96f7d378d740e12051049524d812b3f0a5c5148
Chak10 <francescosorb@live.it>
2017-05-19 09:09:05 +0200

    Update backup.php

--- src/backup.php
+++ src/backup.php
@@ -2,16 +2,6 @@
 
 class backup_dirs {
     
-    /*
-    
-    GNU GENERAL PUBLIC LICENSE
-    Version 3, 29 June 2007
-    Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
-    Everyone is permitted to copy and distribute verbatim copies
-    of this license document, but changing it is not allowed.
-    
-    */
-    
     public $res;
     public $err;
     
